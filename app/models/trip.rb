95bdaf6ca7cd752455c2db18d4bb8981a6a5dc44
Fabio Papa <fabtheman@gmail.com>
2020-01-31 15:31:42 -0800

    Add validations for Trip properties

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,6 @@ class Trip < ApplicationRecord
   belongs_to :tractor
   belongs_to :origin, class_name: :warehouse
   belongs_to :destination, class_name: :warehouse
+
+  validates :date, :driver, :tractor, :origin, :destination
 end
