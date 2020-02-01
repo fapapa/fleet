accb8337c01b1c46de49a1ec6d8d18708ab84cfd
Fabio Papa <fabtheman@gmail.com>
2020-02-01 14:05:19 -0800

    Add driver seeds needed for challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,29 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Create the status seeds
-Status.create(status: 'active')
+active = Status.create(status: 'active')
 Status.create(status: 'inactive')
 Status.create(status: 'suspended')
+
+# --------------------------------------------------------------------------------
+# Seeds to satisfy the tech challenge
+
+# 3 Drivers (all active)
+fabio = Driver.create(first_name: "Fabio", last_name: "Papa", status: active)
+fabio.driver_license = DriverLicense.create(
+  number: "1234567",
+  expiry_date: "January 16, 2022"
+)
+fabio.save
+ali = Driver.create(first_name: "Ali", last_name: "Yazdani", status: active)
+ali.driver_license = DriverLicense.create(
+  number: "7654321",
+  expiry_date: "February 20, 2025"
+)
+ali.save
+john = Driver.create(first_name: "John", last_name: "Smith", status: active)
+john.driver_license = DriverLicense.create(
+  number: "1726354",
+  expiry_date: "December 1, 2021"
+)
+john.save
